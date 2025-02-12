e4cbfc40c611d6472ef0aaa0b6c5037933c6919e
Oleksandr Kuzminskyi <aleks@infrahouse.com>
2025-02-12 13:43:50 -0800

    Exclude DynamoDB based global lock from Vanta scope

--- dns.tf
+++ dns.tf
@@ -1,16 +1,14 @@
 module "update-dns" {
-  source  = "registry.infrahouse.com/infrahouse/update-dns/aws"
-  version = "0.6.1"
-  # insert the 2 required variables here
+  source            = "registry.infrahouse.com/infrahouse/update-dns/aws"
+  version           = "0.7.0"
   asg_name          = var.cluster_name
   route53_zone_id   = data.aws_route53_zone.cluster.zone_id
   route53_public_ip = false
 }
 
 module "update-dns-data" {
-  source  = "registry.infrahouse.com/infrahouse/update-dns/aws"
-  version = "0.6.1"
-  # insert the 2 required variables here
+  source            = "registry.infrahouse.com/infrahouse/update-dns/aws"
+  version           = "0.7.0"
   asg_name          = "${var.cluster_name}-data"
   route53_zone_id   = data.aws_route53_zone.cluster.zone_id
   route53_public_ip = false
