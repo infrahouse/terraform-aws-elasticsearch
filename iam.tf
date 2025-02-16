1d2860b1e56274b01067cd644b28a4374e0256a8
Oleksandr Kuzminskyi <1763754+akuzminsky@users.noreply.github.com>
2025-02-15 16:30:56 -0800

    Elasticsearch cluster uses its own CA and self-signed certificates (#37)
    * The cluster uses its own certificate and CA instead of Let's Encrypt
    * To make it work, Terraform passes secret identifies ca_key_secret and ca_cert_secret as custom puppet facts
    * Make sure puppet modules use this CA to issue self-signed certificates
    * Use new version of update-dns module. It's more stable than old, it uses infrahouse-core classes in the lambda
    * wobsite-pod requires TLS 1.2+
    
    Co-authored-by: Oleksandr Kuzminskyi <aleks@infrahouse.com>

--- iam.tf
+++ iam.tf
@@ -42,6 +42,8 @@ data "aws_iam_policy_document" "elastic_permissions" {
     resources = [
       aws_secretsmanager_secret.elastic.arn,
       aws_secretsmanager_secret.kibana_system.arn,
+      module.ca_cert_secret.secret_arn,
+      module.ca_key_secret.secret_arn,
     ]
   }
   statement {
