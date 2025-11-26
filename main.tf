aafcec2aa4edeb50f377b0537c1dfbd3b44272f3
xOtanix <maksym@tinyfish.io>
2025-11-26 23:13:52 +0200

    update website pod to 5.10.0 for avoid creating extra release

--- main.tf
+++ main.tf
@@ -109,7 +109,7 @@ module "elastic_data_userdata" {
 
 module "elastic_cluster" {
   source  = "registry.infrahouse.com/infrahouse/website-pod/aws"
-  version = "5.8.2"
+  version = "5.10.0"
   providers = {
     aws     = aws
     aws.dns = aws.dns
@@ -173,7 +173,7 @@ module "elastic_cluster_data" {
   # Deploy only if not in the bootstrap mode
   count   = var.bootstrap_mode ? 0 : 1
   source  = "registry.infrahouse.com/infrahouse/website-pod/aws"
-  version = "5.8.2"
+  version = "5.10.0"
   providers = {
     aws     = aws
     aws.dns = aws.dns
